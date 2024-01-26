e330a1b57e71374e81c760e4ac7a7ff061c7d15b
Anwesh43 <anweshthecool0@gmail.com>
2024-01-26 13:33:50 +0530

    adding useAngles

--- src/PureClock.tsx
+++ src/PureClock.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useAngles, useContinuousTime } from "./hooks";
 
 interface PureClockProps {
     color : string 
 }
 
 export const PureClock : React.FC<PureClockProps> = (props : PureClockProps) => {
+    const {h, m, s} = useContinuousTime()
+    const {minuteAngle, hourAngle, secondAngle} = useAngles()
+    console.log("H, M, S", hourAngle, minuteAngle, secondAngle)
+    
     return (
         <div style = {{color : props.color}}>
             Pure Clock 
--- src/hooks.ts
+++ src/hooks.ts
@@ -16,9 +16,21 @@ export const useContinuousTime = () => {
     }, 1000)
 
     return {
-        h: new Date(t).getHours(),
+        h: new Date(t).getHours() % 12,
         m: new Date(t).getMinutes(),
         s: new Date(t).getSeconds()
     }
 }
 
+
+export const useAngles = () => {
+    const {s, h, m} = useContinuousTime()
+    const secondAngle = Math.floor(360 * (s / 60))
+    const minuteAngle = Math.floor(360 * (m * 60 + s) / 3600)
+    const hourAngle = Math.floor(360 * (h * 3600 + m * 60 + s) / (3600 * 12))
+    return {
+        secondAngle,
+        minuteAngle, 
+        hourAngle
+    }
+}
